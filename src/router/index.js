73ba9be546588ab9f1b257935e25c5b704ec0856
liheng <1061555401@qq.com>
2020-09-06 14:18:43 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
-console.log(111111);
+// console.log(111);
 Vue.use(VueRouter);
 
 export default new VueRouter({
