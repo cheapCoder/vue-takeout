9c16c8a0afdd843847d0b0280b82e5d21f63edea
summerwing <henglifire@Gmail.com>
2020-09-06 14:17:12 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
-console.log(321);
+console.log(111111);
 Vue.use(VueRouter);
 
 export default new VueRouter({
